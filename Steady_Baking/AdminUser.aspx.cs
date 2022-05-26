687cb94d2acb3584f21816984ff5f8285d977469
kv_ <tkerven0121@outlook.com>
2022-05-26 17:51:37 +0800

    Navigation Bar

--- Steady_Baking/AdminUser.aspx.cs
+++ Steady_Baking/AdminUser.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Steady_Baking
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class AdminUser : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
