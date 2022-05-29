216c23f2aad80d6f8f0c0d5f73b474690d8ef3c1
MLiFong <mahlifong@gmail.com>
2022-05-30 00:01:53 +0800

    .

--- Steady_Baking/ProfilePage.aspx.designer.cs
+++ Steady_Baking/ProfilePage.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Steady_Baking
     {
 
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
@@ -103,5 +112,14 @@ namespace Steady_Baking
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
