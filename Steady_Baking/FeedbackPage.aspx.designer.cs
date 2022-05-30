5958e69d8696efa276507eac083dd701eaec0d0a
MLiFong <mahlifong@gmail.com>
2022-05-30 21:04:19 +0800

    forgot what i changed

--- Steady_Baking/FeedbackPage.aspx.designer.cs
+++ Steady_Baking/FeedbackPage.aspx.designer.cs
@@ -87,6 +87,15 @@ namespace Steady_Baking
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
 
         /// <summary>
+        /// Label7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
