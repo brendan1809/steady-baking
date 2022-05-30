7f84860df8fcf9a1a3b334bbff4c7cb69f00b720
Brendan Lim <55783210+brendan1809@users.noreply.github.com>
2022-05-30 18:18:30 +0800

    feat: update the admin user edit

--- Steady_Baking/AdminAddUser.aspx.designer.cs
+++ Steady_Baking/AdminAddUser.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace Steady_Baking
     {
 
         /// <summary>
+        /// TitleText control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TitleText;
+
+        /// <summary>
+        /// DesciptionText control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DesciptionText;
+
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
@@ -168,12 +186,12 @@ namespace Steady_Baking
         protected global::System.Web.UI.WebControls.CompareValidator CompareValidator1;
 
         /// <summary>
-        /// AddUser control.
+        /// Submit control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button AddUser;
+        protected global::System.Web.UI.WebControls.Button Submit;
     }
 }
