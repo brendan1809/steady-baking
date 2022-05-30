41a242df6f69e2943d6b0e0a567c1d6ef7423efe
imnafqh <105343512+imnafqh@users.noreply.github.com>
2022-05-30 17:06:39 +0800

    .

---
+++ Steady_Baking/Assets/FAQPage.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Steady_Baking.Assets
+{
+    public partial class FAQPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BulletedList1_Click(object sender, BulletedListEventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BulletedList1_Click1(object sender, BulletedListEventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Steady_Baking/RecipePage.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Content.Master" AutoEventWireup="true" CodeBehind="RecipePage.aspx.cs" Inherits="Steady_Baking.RecipePage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Check out these fun baking ideas, including cakes, breads, bars and more, that you'll be glad to tried!</h1>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+</asp:Content>
